76c60b0b2f10a7c233a7f1d4ba08fb692e88359f
musique88 <musique88181@gmail.com>
2023-09-11 04:20:10 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+use build_html::*;
+use hyper_router::*;
+
+
+async fn add(request: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+
+}
+
+async fn hello(request: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(
+        Full::new(Bytes::from(
+            HtmlPage::new()
+                .with_title(format!("{}!", request.uri()))
+                .with_style(
+                     "p {
+                         color: blue;
+                     }"
+                )
+                .with_container(
+                    Container::new(ContainerType::Div)
+                        .with_paragraph(format!("hello world, look {}", request.uri()))
+                )
+                .to_html_string()
+        ))
+    ))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // We create a TcpListener and bind it to 127.0.0.1:3000
+    let listener = TcpListener::bind(addr).await?;
+
+    // We start a loop to continuously accept incoming connections
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+}
+
